a37c7b61c8f53602fb67c79dd53ce02d1e02f4a6
Mansour Amine <mansour4955amine@gmail.com>
2025-02-18 19:36:44 +0000

    almost finished events

--- src/components/custom/JoinLeaveEvent.jsx
+++ src/components/custom/JoinLeaveEvent.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
-export default function JoinLeaveEvent({ event, setEvent, event }) {
+export default function JoinLeaveEvent({ mode, setEvent, event }) {
+  const { t } = useTranslation();
+  const myId = "67b396ab55bc92ee3bd4094d";
+  const isUSerParticipant = event.participants.find(
+    (user) => user._id === myId
+  );
   return (
     <div className="max-xs:text-xs xs:text-xs sm:text-sm lg:text-base">
-      JoinLeaveEvent
+      {isUSerParticipant ? (
+        <span
+          className={`py-0.5 px-2 rounded-md ${mode === "light" ? "bg-light-red" : "bg-dark-red"}`}
+        >
+          {t("words.leave")}
+        </span>
+      ) : (
+        <span
+          className={`py-0.5 px-2 rounded-md ${
+            mode === "light" ? "bg-light-secondary" : "bg-dark-secondary"
+          }`}
+        >
+          {t("words.join")}
+        </span>
+      )}
     </div>
   );
 }
